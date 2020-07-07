9e1e3c27c28874e16ad3a4c4c91bf9f4b1b09915
alec dye <alecdye@gmail.com>
2020-07-07 16:32:37 -0700

    Correct args

--- src/11_args.py
+++ src/11_args.py
@@ -18,18 +18,7 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 def f2(*args):
-    if type(args) == int or tuple:
-        return sum(args)
-        # c = 0
-        # for num in args:
-        #     c + num
-        # return c
-    else:
-        c = 0
-        for item in args:
-            c += item
-        return c
-    # return sum(args)
+    return sum(args)
 
 
 print(f2(1))  # Should print 1
@@ -39,7 +28,7 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
 a = [7, 6, 5, 4]
 # How do you have to modify the f2 call below to make this work?
-# print(f2(a))  # Should print 22
+print(f2(*a))  # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -65,7 +54,10 @@ print(f3(8))  # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
-# def f4(a, b):
+def f4(**kwargs):
+    for key, value in kwargs.items():
+        print("{0} = {1}".format(key, value))
+
 
 # Should print
 # key: a, value: 12
@@ -81,4 +73,4 @@ f4(city="Berkeley", population=121240, founded="March 23, 1868")
 d = {"monster": "goblin", "hp": 3}
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(monster="goblin", hp=3)
