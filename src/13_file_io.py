24bf59faef0ca37892a4cd64ac0cb4dd5acddbf4
alec dye <alecdye@gmail.com>
2020-07-07 15:45:52 -0700

    Complete remaining challenges

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -4,12 +4,19 @@ at how to read and write to files here:
 
 https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 """
+import sys
+import os
 
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+with open(os.path.join(sys.path[0], "foo.txt")) as f:
+    read_data = f.read()
+    print(read_data)
+    f.closed
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +24,8 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+newFile = open("bar.txt", "w")
+newFile.write("A cool new look")
+newFile.write("for your summer collection")
+newFile.write("...hotdogs")
+newFile.close()
